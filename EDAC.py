c995df2146a0d5b26b95566bf1979440bbf1f7d7
Rafael Milla-Koch <rafael.millakoch@gmail.com>
2026-02-22 14:18:45 +0100

    in the strug

--- EDAC.py
+++ EDAC.py
@@ -2,6 +2,27 @@ import numpy as np
 
 
 # UART parity
+class Parity:
+    def __init__(self, data=b''):
+        self.parity_key = 0
+        self.data = data
+
+    def parity_calc(self):
+        for byte in self.data:
+            self.parity_key <<= 1
+            if byte.bit_count()%2 != 0:
+                self.parity_key += 1
+        nr_bytes = (len(self.data) + 7) // 8
+        return list(self.parity_key.to_bytes(nr_bytes)), nr_bytes, bin(self.parity_key)
+
+    def check_parity(self, dirty, nr_parity_bytes):
+        for byte in dirty[:-nr_parity_bytes]:
+            break
+
+
+
+
+
 
 
 # Fletcher
--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from reedsolo import *
 dataset = DataGenerator(18, 8, 10)
 clean = dataset.generate_clean()
 
+#Parity
+data = [246, 111, 119,  81, 205, 212,  67,  28,  75, 106, 168,  16, 180, 185, 117, 209, 128,  78]
+print(clean)
+print(Parity(clean).parity_calc())
+
 #Fletcher
 fl_instance = FletcherChecksumBytes
 fl_codeword = fl_instance.get_fletcher16(bytes(clean))
 print(fl_codeword)
-
-
-
-
 
 #CRC
 crc_config = crc.Configuration(
